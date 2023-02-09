a5826d04323ebf10dad1228e3ed5eb4074ed1d14
Emmanuel Adepoju <femiemmanuel1990@gmail.com>
2023-02-09 17:58:06 +0100

    Added Functions module and  signal to json conversion script for goldscalper

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 from credentials import Token
 from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
-from responses import start, reverse
+from responses import start, reverse, raw
 
 print("bot has started running")
 # Creating a logger to be able to monitor bot processes
@@ -17,9 +17,11 @@ if __name__ == '__main__':
     app = ApplicationBuilder().token(Token).build()
 
     start_handler = CommandHandler('start', start)
-    message_handler = MessageHandler(filters.TEXT, reverse)
+    # message_handler = MessageHandler(filters.TEXT, reverse)
+    raw_message_handler = MessageHandler(filters.TEXT, raw)
     app.add_handler(start_handler)
-    app.add_handler(message_handler)
+    # app.add_handler(message_handler)
+    app.add_handler(raw_message_handler)
 
     app.run_polling()
     print("Bot is running...")
--- responses.py
+++ responses.py
@@ -1,6 +1,8 @@
 from datetime import datetime as dt
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from functions import call_openai_api, parse_trade_info_goldscalper
+
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -15,3 +17,15 @@ async def reverse(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update._effective_chat.id, text=reversed_text
         )
+        
+async def raw(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_input = update.message.text
+    print(user_input)
+
+    # Send Trading Signal to Chatgpt API
+    prompt = parse_trade_info_goldscalper(user_input)
+
+    await context.bot.send_message(
+        chat_id=update._effective_chat.id, text=prompt
+        )
+
